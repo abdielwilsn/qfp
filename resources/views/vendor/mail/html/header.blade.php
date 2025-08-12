6f69ca22ae04a8f52a8528aabc8c4704a95e8cd8
Abdiel Wilson <abdieljohnwilson@gmail.com>
2025-08-12 06:52:26 +0100

    wip

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+@if (trim($slot) === 'Starbiit')
+{{-- <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Starbiit Logo"> --}}
 @else
 {{ $slot }}
 @endif
